36989398864bef6d7b9bb52f07534888ff0e8806
Ronald / <belloconcepcionr@gmail.com>
2025-12-14 17:23:25 -0500

    Apis actualizadas

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from libs.system_info import get_system_info
 from libs.port_utils import runPort
+from libs.wifi_utils import connect_to_network, scan_networks
 
 app = FastAPI()
 
@@ -24,3 +25,16 @@ async def openPort(
 ):
     server = runPort(port, loc)
     return server
+
+@app.get("/api/wifi/scan")
+async def scanWifi():
+    networks = scan_networks()
+    return networks
+
+@app.post("/api/wifi/connect")
+async def connectWifi(
+    ssid: str = Form(...),
+    pswd: str = Form(...)
+):
+    connect = connect_to_network(ssid, pswd)
+    return connect
